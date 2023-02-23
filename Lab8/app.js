cf556c2a3767f6a89faadfd550ca3a8d7671e132
bromaeg <a01709653@tec.mx>
2023-02-23 13:58:40 -0600

    Codigo

--- Lab8/app.js
+++ Lab8/app.js
@@ -4,3 +4,42 @@ console.log("Hola desde node");
 
 const filesystem = require("fs");
 filesystem.writeFileSync("hola.txt", "Hola desde node");
+
+
+let imprimir = (numero) =>{
+    console.log(numero);
+}
+
+const arreglo = [5000, 60,61,90,100,10,20,100];
+
+for (let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
+
+setTimeout(() => {
+    console.log("hackeado");
+}, 1000);
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Mi Pagina Web desde Servidor</title></head>");
+    res.write("<body><h1>Hola desde mi servidor privado</h1></body>");
+    res.end("Holiiiiiis");
+});
+
+function calcularPromedio(arr) {
+    const suma = arr.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
+    const promedio = suma / arr.length;
+    return promedio;
+  }
+const numeros = [2, 4, 6, 8, 10];
+const promedio = calcularPromedio(numeros);
+console.log(promedio);
+server.listen(3000);
+
